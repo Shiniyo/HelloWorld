3704adc0dae0a79ad0396a5d76f1c2d146d745aa
Vinicius <shiniyoasakura@gmail.com>
2018-11-22 18:08:16 -0300

    Primeiro código

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    
+    public static void main(String [] args){
+        System.out.println("Olá Mundo!!");
+        System.out.println("Daqui a pouco é 2019");
+    }
+}
